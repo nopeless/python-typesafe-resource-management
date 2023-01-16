d5d6ed13986014e67e9b322bc9f0b2458288eb2b
nopeless <38830903+nopeless@users.noreply.github.com>
2023-01-15 23:40:26 -0600

    update demo

--- demo/run.py
+++ demo/run.py
@@ -4,24 +4,21 @@ import pathlib
 
 d = str(pathlib.Path(__file__).parent.resolve()) + os.sep
 sys.path.insert(0, str(pathlib.Path(__file__).parent.joinpath("../src").resolve()))
+os.chdir(d)
+# ^ Not needed
 # ------------------------------------------------------
-# Example
-from pathtyped import (
-    ResourceManager,
-    DefinitionFile,
-    Loaders,
-    group_by,
-    remove_known_extensions,
-)
+# Example code
 
-from resource_definition import root
+from pathtyped import *
 
 rm = ResourceManager(
-    f"{d}resources",
-    DefinitionFile("", f"{d}resource_definition.py"),
+    f"resources",
+    DefinitionFile("", f"resource_definition.py"),
     [remove_known_extensions(r"txt|json"), group_by(r"(speech)_(\d)")],
     [Loaders.text, Loaders.json],
 )
+
+from resource_definition import root
 
 resource: root = rm.root  # type: ignore
 
